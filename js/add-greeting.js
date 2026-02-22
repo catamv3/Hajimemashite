7afb8564c97f2ab6f318e86258e80bbbfa25d236
catamv3 <michaelcatalanotti4401@gmail.com>
2026-02-21 22:32:30 -0500

    initial commit! 最初のコミット！

---
+++ js/add-greeting.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if(hourNow > 18) {
+    greeting = "こんばんは!";
+} else if (hourNow > 12) {
+    greeting = "こんにちは!";
+} else if (hourNow > 0){
+    greeting = "おはようございます!";
+} else {
+    greeting = "Welcome!"
+}
+
+document.write('<h3>' + greeting + '</h3>');
